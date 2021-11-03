91d473aec6e9ec85d7fd471f95996f81c91d3ecf
Vincent Le <quyen.lechi@gmail.com>
2021-11-02 20:15:21 -0700

    Initial Commit

---
+++ Shared/SugoiMangaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SugoiMangaApp.swift
+//  Shared
+//
+//  Created by Chi-Quyen Le on 2021-11-02.
+//
+
+import SwiftUI
+
+@main
+struct SugoiMangaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
